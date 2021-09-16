0e30113c52cdda1e09b130778b978d20615fec36
Pablo Lopez <pablo.lopez@kolbit.es>
2021-09-16 13:29:39 +0200

    Version con dos mensajes

--- es.kolbit.prueba.git/src/es/kolbit/prueba/git/GitTest.java
+++ es.kolbit.prueba.git/src/es/kolbit/prueba/git/GitTest.java
@@ -3,5 +3,6 @@ package es.kolbit.prueba.git;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("He modificado el código!!!");
+		System.out.println("Ahora he agregado nuevo código");
 	}
 }
