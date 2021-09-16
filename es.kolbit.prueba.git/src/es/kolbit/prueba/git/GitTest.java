0f182561f704c9b1e4ad2066341fbbaf11a4c16d
Pablo Lopez <pablo.lopez@kolbit.es>
2021-09-16 17:36:16 +0200

    Otra lines depues de subir a hithub

--- es.kolbit.prueba.git/src/es/kolbit/prueba/git/GitTest.java
+++ es.kolbit.prueba.git/src/es/kolbit/prueba/git/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("He modificado el código!!!");
 		System.out.println("Ahora he agregado nuevo código");
+		
+		System.out.println("Estos cambios son despues de subir hithub");
 	}
 }
